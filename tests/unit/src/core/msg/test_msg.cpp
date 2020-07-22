3d47777db45713a05bc1faa40a661f1c4996bd24
darkopancev <darkopancev.dev@gmail.com>
2020-07-22 16:46:17 +0800

    add multiple msg test with issue

--- tests/unit/src/core/msg/test_msg.cpp
+++ tests/unit/src/core/msg/test_msg.cpp
@@ -543,5 +543,180 @@ TEST_F(TestMsg, singleSendAndReceiveMsg)
 
 TEST_F(TestMsg, multipleSendReceiveMsg)
 {
-    // TODO
+    mtDEFINE_ALIGNED_VAR(buffer, sizeof(Instance), uint64_t);
+
+    uint32_t size = mtARRAY_LENGTH(buffer);
+
+    Instance instance = Instance::Init((void *)buffer, (size_t *)&size);
+
+    EXPECT_TRUE(instance.IsInitialized());
+
+    EXPECT_EQ(instance.Get<ThreadScheduler>().GetNumOfThreadsInScheduler(), 0);
+    EXPECT_EQ(instance.Get<ThreadScheduler>().GetCurrentActiveThread(), nullptr);
+    EXPECT_EQ(instance.Get<ThreadScheduler>().GetCurrentActivePid(), KERNEL_PID_UNDEF);
+
+    char idleStack[128];
+
+    Thread *idleThread = Thread::Init(instance, idleStack, sizeof(idleStack), 15,
+                                      THREAD_FLAGS_CREATE_WOUT_YIELD | \
+                                      THREAD_FLAGS_CREATE_STACKMARKER,
+                                      NULL, NULL, "idle");
+
+    EXPECT_NE(idleThread, nullptr);
+
+    EXPECT_EQ(idleThread->GetPid(), 1);
+    EXPECT_EQ(idleThread->GetPriority(), 15);
+    EXPECT_EQ(idleThread->GetName(), "idle");
+    EXPECT_EQ(idleThread->GetStatus(), THREAD_STATUS_PENDING);
+
+    char mainStack[128];
+
+    Thread *mainThread = Thread::Init(instance, mainStack, sizeof(mainStack), 7,
+                                      THREAD_FLAGS_CREATE_WOUT_YIELD | \
+                                      THREAD_FLAGS_CREATE_STACKMARKER,
+                                      NULL, NULL, "main");
+
+    EXPECT_NE(mainThread, nullptr);
+
+    EXPECT_EQ(mainThread->GetPid(), 2);
+    EXPECT_EQ(mainThread->GetPriority(), 7);
+    EXPECT_EQ(mainThread->GetName(), "main");
+    EXPECT_EQ(mainThread->GetStatus(), THREAD_STATUS_PENDING);
+
+    char task1Stack[128];
+
+    Thread *task1Thread = Thread::Init(instance, task1Stack, sizeof(task1Stack), 5,
+                                       THREAD_FLAGS_CREATE_WOUT_YIELD | \
+                                       THREAD_FLAGS_CREATE_STACKMARKER,
+                                       NULL, NULL, "task1");
+
+    EXPECT_NE(task1Thread, nullptr);
+
+    EXPECT_EQ(task1Thread->GetPid(), 3);
+    EXPECT_EQ(task1Thread->GetPriority(), 5);
+    EXPECT_EQ(task1Thread->GetName(), "task1");
+    EXPECT_EQ(task1Thread->GetStatus(), THREAD_STATUS_PENDING);
+
+    EXPECT_EQ(instance.Get<ThreadScheduler>().GetNumOfThreadsInScheduler(), 3);
+    EXPECT_EQ(instance.Get<ThreadScheduler>().GetThreadFromScheduler(idleThread->GetPid()), idleThread);
+    EXPECT_EQ(instance.Get<ThreadScheduler>().GetThreadFromScheduler(mainThread->GetPid()), mainThread);
+    EXPECT_EQ(instance.Get<ThreadScheduler>().GetThreadFromScheduler(task1Thread->GetPid()), task1Thread);
+    EXPECT_FALSE(instance.Get<ThreadScheduler>().IsContextSwitchRequestedFromIsr());
+    EXPECT_EQ(instance.Get<ThreadScheduler>().GetCurrentActiveThread(), nullptr);
+    EXPECT_EQ(instance.Get<ThreadScheduler>().GetCurrentActivePid(), KERNEL_PID_UNDEF);
+
+    EXPECT_EQ(mainThread->GetStatus(), THREAD_STATUS_PENDING);
+    EXPECT_EQ(idleThread->GetStatus(), THREAD_STATUS_PENDING);
+    EXPECT_EQ(task1Thread->GetStatus(), THREAD_STATUS_PENDING);
+
+    instance.Get<ThreadScheduler>().Run();
+
+    EXPECT_EQ(mainThread->GetStatus(), THREAD_STATUS_PENDING);
+    EXPECT_EQ(idleThread->GetStatus(), THREAD_STATUS_PENDING);
+    EXPECT_EQ(task1Thread->GetStatus(), THREAD_STATUS_RUNNING);
+
+    EXPECT_EQ(instance.Get<ThreadScheduler>().GetCurrentActiveThread(), task1Thread);
+    EXPECT_EQ(instance.Get<ThreadScheduler>().GetCurrentActivePid(), task1Thread->GetPid());
+    EXPECT_EQ(instance.Get<ThreadScheduler>().GetNumOfThreadsInScheduler(), 3);
+
+    /**
+     * -------------------------------------------------------------------------
+     * [TEST CASE] create msg queue in current active thread
+     * -------------------------------------------------------------------------
+     **/
+
+    EXPECT_EQ(task1Thread->HasMsgQueue(), 0);
+
+    Msg task1MsgArray[16];
+
+    for (int i = 0; i < 16; i++)
+    {
+        task1MsgArray[i].Init(instance);
+    }
+
+    task1Thread->InitMsgQueue(task1MsgArray, mtARRAY_LENGTH(task1MsgArray));
+
+    EXPECT_EQ(task1Thread->HasMsgQueue(), 1);
+    EXPECT_EQ(task1Thread->GetNumOfMsgInQueue(), 0);
+
+    /**
+     * -------------------------------------------------------------------------
+     * [TEST CASE] set current active thread to sleep status and send msg to it,
+     * it expected to queued the msg.
+     * -------------------------------------------------------------------------
+     **/
+
+    instance.Get<ThreadScheduler>().SetThreadStatusAndUpdateRunqueue(task1Thread, THREAD_STATUS_SLEEPING);
+
+    EXPECT_EQ(mainThread->GetStatus(), THREAD_STATUS_PENDING);
+    EXPECT_EQ(idleThread->GetStatus(), THREAD_STATUS_PENDING);
+    EXPECT_EQ(task1Thread->GetStatus(), THREAD_STATUS_SLEEPING);
+
+    instance.Get<ThreadScheduler>().Run();
+
+    EXPECT_EQ(mainThread->GetStatus(), THREAD_STATUS_RUNNING);
+    EXPECT_EQ(idleThread->GetStatus(), THREAD_STATUS_PENDING);
+    EXPECT_EQ(task1Thread->GetStatus(), THREAD_STATUS_SLEEPING);
+
+    Msg msg1 = Msg(instance);
+    Msg msg2 = Msg(instance);
+    Msg msg3 = Msg(instance);
+    Msg msg4 = Msg(instance);
+
+    msg1.mType = 0xff;
+    msg1.mContent.mValue = 0x1;
+
+    msg2.mType = 0xff;
+    msg2.mContent.mValue = 0x2;
+
+    msg3.mType = 0xff;
+    msg3.mContent.mValue = 0x3;
+
+    msg4.mType = 0xff;
+    msg4.mContent.mValue = 0x4;
+
+    EXPECT_EQ(msg1.Send(task1Thread->GetPid()), 1);
+    EXPECT_EQ(msg2.Send(task1Thread->GetPid()), 1);
+    EXPECT_EQ(msg3.Send(task1Thread->GetPid()), 1);
+    EXPECT_EQ(msg4.Send(task1Thread->GetPid()), 1);
+
+    /* Note: task1Thread is in sleeping status and has message queue, so the
+     * message sent to task1Thread should be queued */
+
+    EXPECT_EQ(task1Thread->GetNumOfMsgInQueue(), 4);
+
+    /* Note: set task1Thread to running status and dequeque the message */
+
+    instance.Get<ThreadScheduler>().SetThreadStatusAndUpdateRunqueue(task1Thread, THREAD_STATUS_PENDING);
+
+    EXPECT_EQ(mainThread->GetStatus(), THREAD_STATUS_RUNNING);
+    EXPECT_EQ(idleThread->GetStatus(), THREAD_STATUS_PENDING);
+    EXPECT_EQ(task1Thread->GetStatus(), THREAD_STATUS_PENDING);
+
+    instance.Get<ThreadScheduler>().Run();
+
+    EXPECT_EQ(mainThread->GetStatus(), THREAD_STATUS_PENDING);
+    EXPECT_EQ(idleThread->GetStatus(), THREAD_STATUS_PENDING);
+    EXPECT_EQ(task1Thread->GetStatus(), THREAD_STATUS_RUNNING);
+
+    EXPECT_EQ(task1Thread->GetNumOfMsgInQueue(), 4);
+
+    Msg msg1InTask1 = Msg(instance);
+    //Msg msg2InTask1 = Msg(instance);
+
+    EXPECT_EQ(msg1InTask1.Receive(), 1);
+
+    EXPECT_EQ(msg1InTask1.mSenderPid, mainThread->GetPid());
+    EXPECT_EQ(msg1InTask1.mType, 0xff);
+    EXPECT_EQ(msg1InTask1.mContent.mValue, 0x01); /* get msg1 */
+
+    EXPECT_EQ(msg1InTask1.Receive(), 1);
+
+    //EXPECT_EQ(msg2InTask1.Receive(), 1);
+
+    //EXPECT_EQ(msg2InTask1.mSenderPid, mainThread->GetPid());
+    //EXPECT_EQ(msg2InTask1.mType, 0xff);
+    //EXPECT_EQ(msg2InTask1.mContent.mValue, 0x02); /* get msg2 */
+
+
 }
