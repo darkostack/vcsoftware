1c0e9e4f62ba1813917a19592b386bdb61e21e0e
darkopancev <darkopancev.dev@gmail.com>
2020-08-12 15:37:45 +0800

    add back OS_BRAND, used for optimized link flag

--- platform/SDK/Vertexcom_vcrtos_vc7300/Vertexcom_vcrtos_vc7300.cmake
+++ platform/SDK/Vertexcom_vcrtos_vc7300/Vertexcom_vcrtos_vc7300.cmake
@@ -1,5 +1,8 @@
+# CROSS COMPILER SETTINGS
 cmake_minimum_required (VERSION 3.5)
 SET(CMAKE_SYSTEM_NAME Generic)
+
+set(OS_BRAND VertexcomRTOS)
 
 set(CMAKE_EXE_LINKER_FLAGS_DEBUG
     "${CMAKE_EXE_LINKER_FLAGS_DEBUG} -T${CMAKE_SOURCE_DIR}/platform/SDK/Vertexcom_vcrtos_vc7300/vc7300.ld -static"
