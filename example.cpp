bf8f2ce72d1249a47c40138e6645e1a381d7f421
darkopancev <darkopancev.dev@gmail.com>
2020-08-13 15:08:39 +0800

    add timer test example

--- example.cpp
+++ example.cpp
@@ -2,9 +2,23 @@
 
 #include <vcrtos/instance.h>
 #include <vcrtos/thread.h>
+#include <vcrtos/xtimer.h>
 #include <vcrtos/cli.h>
 
 static instance_t *_instance;
+static xtimer_t timer_test;
+static uint32_t counter = 0;
+
+void xtimer_test_handler(void *arg)
+{
+    uint32_t *cnt = static_cast<uint32_t *>(arg);
+
+    *cnt += 1;
+
+    printf("%lu\r\n", static_cast<uint32_t>(*cnt));
+
+    xtimer_set(&timer_test, 1000000);
+}
 
 void setup(void)
 {
@@ -15,8 +29,10 @@ void setup(void)
     _instance = instance_get();
 
     vccli_uart_init(_instance);
-}
 
+    xtimer_init(_instance, &timer_test, xtimer_test_handler, static_cast<void *>(&counter));
+    xtimer_set(&timer_test, 1000000);
+}
 
 void loop(void)
 {
