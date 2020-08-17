f6bed2c2fa6a8ec58f0e5df81e68a7dde8c0fe56
darkopancev <darkopancev.dev@gmail.com>
2020-08-17 21:56:32 +0800

    rename project folder name in cmake

--- platform/SDK/Vertexcom_vcrtos_vc7300/Vertexcom_vcrtos_vc7300.cmake
+++ platform/SDK/Vertexcom_vcrtos_vc7300/Vertexcom_vcrtos_vc7300.cmake
@@ -2,14 +2,14 @@
 cmake_minimum_required (VERSION 3.5)
 SET(CMAKE_SYSTEM_NAME Generic)
 
-set(OS_BRAND VertexcomRTOS)
+set(OS_BRAND vcrtos)
 
 set(CMAKE_EXE_LINKER_FLAGS_DEBUG
-    "${CMAKE_EXE_LINKER_FLAGS_DEBUG} -T${CMAKE_SOURCE_DIR}/platform/SDK/Vertexcom_vcrtos_vc7300/vc7300.ld -static"
+    "${CMAKE_EXE_LINKER_FLAGS_DEBUG} -T${CMAKE_SOURCE_DIR}/platform/SDK/vertexcom_vcrtos_vc7300/vc7300.ld -static"
 )
 
 set(CMAKE_EXE_LINKER_FLAGS_RELEASE
-    "${CMAKE_EXE_LINKER_FLAGS_RELEASE} -T${CMAKE_SOURCE_DIR}/platform/SDK/Vertexcom_vcrtos_vc7300/vc7300.ld -static"
+    "${CMAKE_EXE_LINKER_FLAGS_RELEASE} -T${CMAKE_SOURCE_DIR}/platform/SDK/vertexcom_vcrtos_vc7300/vc7300.ld -static"
 )
 
-set(EXTRA_CMAKE_DIRS ${EXTRA_CMAKE_DIRS} "${CMAKE_SOURCE_DIR}/platform/SDK/Vertexcom_vcrtos_vc7300")
+set(EXTRA_CMAKE_DIRS ${EXTRA_CMAKE_DIRS} "${CMAKE_SOURCE_DIR}/platform/SDK/vertexcom_vcrtos_vc7300")
