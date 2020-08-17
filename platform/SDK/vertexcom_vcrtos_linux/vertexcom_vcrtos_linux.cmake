095f24e3ad6ce36f81a64661720f0530a83baabc
darkopancev <darkopancev.dev@gmail.com>
2020-08-17 23:29:36 +0800

    add linux basic files

---
+++ platform/SDK/vertexcom_vcrtos_linux/vertexcom_vcrtos_linux.cmake
@@ -0,0 +1,7 @@
+# CROSS COMPILER SETTINGS
+cmake_minimum_required (VERSION 3.5)
+SET(CMAKE_SYSTEM_NAME Generic)
+
+set(OS_BRAND Linux)
+
+set(EXTRA_CMAKE_DIRS ${EXTRA_CMAKE_DIRS} "${CMAKE_SOURCE_DIR}/platform/SDK/vertexcom_vcrtos_linux")
