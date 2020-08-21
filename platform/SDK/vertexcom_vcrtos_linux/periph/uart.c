04236d1358ae0eaefbffaf3cefe0cdc72207a383
darko pancev <darkopancev.dev@gmail.com>
2020-08-21 02:29:09 +0000

    add initial uart implementation

---
+++ platform/SDK/vertexcom_vcrtos_linux/periph/uart.c
@@ -0,0 +1,12 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <string.h>
+#include <termios.h>
+#include <fcntl.h>
+
+#include <vcdrivers/periph/uart.h>
+
+#include "native_internal.h"
+#include "async_read.h"
+
