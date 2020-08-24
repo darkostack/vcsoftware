6b79801550f61d5ee89a03d14062464d58b7c885
darkopancev <darkopancev.dev@gmail.com>
2020-08-24 13:47:59 +0800

    add ztimer test

--- vertexcom_vcrtos_vc7300.cpp
+++ vertexcom_vcrtos_vc7300.cpp
@@ -11,6 +11,17 @@ const cli_command_t user_command_list[] = {
     { "ps", vcrtos_cmd_ps },
 };
 
+static ztimer_t _timer1;
+static uint32_t _timer1_counter = 0;
+
+static void _timer1_handler(void *arg)
+{
+    uint32_t *counter = static_cast<uint32_t *>(arg);
+    *counter += 1;
+    printf("counter: %lu\r\n", static_cast<uint32_t>(*counter));
+    ztimer_set(ZTIMER_USEC, &_timer1, 1000000);
+}
+
 void Arduino::setup(void)
 {
     Serial.begin(115200);
@@ -19,10 +30,14 @@ void Arduino::setup(void)
 
     vccli_uart_init(instance);
     vccli_set_user_commands(user_command_list, 1);
+
+    _timer1.callback = _timer1_handler;
+    _timer1.arg = &_timer1_counter;
+
+    ztimer_set(ZTIMER_USEC, &_timer1, 1000000);
 }
 
 void Arduino::loop(void)
 {
-    printf("now: %lu\r\n", ztimer_now(ZTIMER_USEC));
-    thread_yield(instance);
+    thread_sleep(instance);
 }
