0d2df190b8456be0a9a6807870ad9fb2d9206aea
darkopancev <darkopancev.dev@gmail.com>
2020-08-26 14:43:27 +0800

    use vctim_set_absolute in timer set function

--- vertexcom_vcrtos_vc7300.cpp
+++ vertexcom_vcrtos_vc7300.cpp
@@ -5,20 +5,27 @@
 #include <vcrtos/ztimer.h>
 #include <vcrtos/cli.h>
 
+static ztimer_t _timer1;
+static uint32_t _timer1_counter = 0;
+
 extern "C" void vcrtos_cmd_ps(int argc, char **argv);
+
+void _get_timer_counter(int argc, char **argv)
+{
+    (void) argc;
+    (void) argv;
+    printf("counter: %lu\r\n", _timer1_counter);
+}
 
 const cli_command_t user_command_list[] = {
     { "ps", vcrtos_cmd_ps },
+    { "tc", _get_timer_counter },
 };
-
-static ztimer_t _timer1;
-static uint32_t _timer1_counter = 0;
 
 static void _timer1_handler(void *arg)
 {
     uint32_t *counter = static_cast<uint32_t *>(arg);
     *counter += 1;
-    printf("counter: %lu\r\n", static_cast<uint32_t>(*counter));
     ztimer_set(ZTIMER_USEC, &_timer1, 1000000);
 }
 
@@ -29,7 +36,7 @@ void Arduino::setup(void)
     Serial.println("Enable command line interface");
 
     vccli_uart_init(instance);
-    vccli_set_user_commands(user_command_list, 1);
+    vccli_set_user_commands(user_command_list, 2);
 
     _timer1.callback = _timer1_handler;
     _timer1.arg = &_timer1_counter;
