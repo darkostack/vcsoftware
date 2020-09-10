865842ad9d93f06bb2e8ecba0c2f7b6c09c3a8fd
darkopancev <darkopancev.dev@gmail.com>
2020-09-10 15:43:52 +0800

    enable etimer test in vcrtos_processes.c

--- vcrtos_processes.c
+++ vcrtos_processes.c
@@ -12,8 +12,8 @@ PROCESS(app_process, "app-process", VCRTOS_CONFIG_MAIN_THREAD_STACK_SIZE);
 
 AUTOSTART_PROCESSES(&app_process);
 
-//static struct etimer timer_etimer;
-//static uint32_t etimer_counter = 0;
+static struct etimer timer_etimer;
+static uint32_t etimer_counter = 0;
 
 PROCESS_THREAD(app_process, ev, data)
 {
@@ -21,17 +21,13 @@ PROCESS_THREAD(app_process, ev, data)
 
     printf("app-process start\r\n");
 
-    //etimer_set(&timer_etimer, CLOCK_SECOND);
+    etimer_set(&timer_etimer, CLOCK_SECOND);
 
     while (1)
     {
-        PROCESS_YIELD();
-
-        (void) ev;
-
-        //PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
-        //etimer_set(&timer_etimer, CLOCK_SECOND);
-        //printf("e: %" PRIu32 "\r\n", etimer_counter++);
+        PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
+        etimer_set(&timer_etimer, CLOCK_SECOND);
+        printf("e: %" PRIu32 "\r\n", etimer_counter++);
     }
 
     PROCESS_END();
