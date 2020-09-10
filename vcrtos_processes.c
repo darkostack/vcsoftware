03b669dc3aa8f3120e4fbff51d1ca6bbb2c2e86b
darkopancev <darkopancev.dev@gmail.com>
2020-09-10 11:17:17 +0800

    use pure event based for contiki process

--- vcrtos_processes.c
+++ vcrtos_processes.c
@@ -12,18 +12,8 @@ PROCESS(app_process, "app-process", VCRTOS_CONFIG_MAIN_THREAD_STACK_SIZE);
 
 AUTOSTART_PROCESSES(&app_process);
 
-static struct etimer timer_etimer;
-static uint32_t etimer_counter = 0;
-
-static struct ctimer timer_ctimer;
-static uint32_t ctimer_counter = 0;
-
-static void _ctimer_handler(void *arg)
-{
-    struct ctimer *ct = (struct ctimer *)arg;
-    ctimer_counter += 1;
-    ctimer_reset(ct);
-}
+//static struct etimer timer_etimer;
+//static uint32_t etimer_counter = 0;
 
 PROCESS_THREAD(app_process, ev, data)
 {
@@ -31,15 +21,17 @@ PROCESS_THREAD(app_process, ev, data)
 
     printf("app-process start\r\n");
 
-    etimer_set(&timer_etimer, CLOCK_SECOND);
-    ctimer_set(&timer_ctimer, CLOCK_SECOND / 2, _ctimer_handler, &timer_ctimer);
+    //etimer_set(&timer_etimer, CLOCK_SECOND);
 
     while (1)
     {
-        PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
-        etimer_set(&timer_etimer, CLOCK_SECOND);
-        printf("e: %" PRIu32 "\r\n", etimer_counter++);
-        printf("c: %" PRIu32 "\r\n", ctimer_counter);
+        PROCESS_YIELD();
+
+        (void) ev;
+
+        //PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
+        //etimer_set(&timer_etimer, CLOCK_SECOND);
+        //printf("e: %" PRIu32 "\r\n", etimer_counter++);
     }
 
     PROCESS_END();
