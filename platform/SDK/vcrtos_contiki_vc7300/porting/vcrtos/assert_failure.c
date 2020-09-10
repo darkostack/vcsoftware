e52bebe46f0a74d19da84d7593a97845bddc756b
darkopancev <darkopancev.dev@gmail.com>
2020-09-10 17:53:13 +0800

    add copyright notice

--- platform/SDK/vcrtos_contiki_vc7300/porting/vcrtos/assert_failure.c
+++ platform/SDK/vcrtos_contiki_vc7300/porting/vcrtos/assert_failure.c
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2020, Vertexcom Technologies, Inc.
+ * All rights reserved.
+ *
+ * NOTICE: All information contained herein is, and remains
+ * the property of Vertexcom Technologies, Inc. and its suppliers,
+ * if any. The intellectual and technical concepts contained
+ * herein are proprietary to Vertexcom Technologies, Inc.
+ * and may be covered by U.S. and Foreign Patents, patents in process,
+ * and protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Vertexcom Technologies, Inc.
+ *
+ * Authors: Darko Pancev <darko.pancev@vertexcom.com>
+ */
+
 #include <stdio.h>
 #include <vcrtos/assert.h>
 
