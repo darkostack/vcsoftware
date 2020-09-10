5eab8155682f8bd7ceee19d653705d9fbf12dc16
darkopancev <darkopancev.dev@gmail.com>
2020-09-10 18:24:42 +0800

    remove event thread from kernel init

--- platform/SDK/vcrtos_vc7300/porting/vcrtos/kernel_init.c
+++ platform/SDK/vcrtos_vc7300/porting/vcrtos/kernel_init.c
@@ -31,10 +31,6 @@
 #include <vcrtos/ztimer/periph_timer.h>
 #endif
 
-#if VCRTOS_CONFIG_THREAD_EVENT_ENABLE
-#include <vcrtos/event.h>
-#endif
-
 #if VCRTOS_CONFIG_ZTIMER_ENABLE
 #define WIDTH_TO_MAXVAL(width) (UINT32_MAX >> (32 - width))
 static ztimer_periph_timer_t _ztimer_periph_timer_usec = {
@@ -73,55 +69,6 @@ void *thread_idle_handler(void *arg)
 char _main_stack[VCRTOS_CONFIG_MAIN_THREAD_STACK_SIZE];
 char _idle_stack[VCRTOS_CONFIG_IDLE_THREAD_STACK_SIZE];
 
-#if VCRTOS_CONFIG_THREAD_EVENT_ENABLE
-
-void *thread_event_handler(void *arg)
-{
-    event_queue_t *queue = (event_queue_t *)arg;
-
-    event_queue_claim(queue);
-    event_loop(queue);
-
-    return NULL;
-}
-
-event_queue_t event_queue_highest;
-char event_queue_highest_stack[VCRTOS_CONFIG_THREAD_EVENT_HIGHEST_STACK_SIZE];
-
-event_queue_t event_queue_medium;
-char event_queue_medium_stack[VCRTOS_CONFIG_THREAD_EVENT_MEDIUM_STACK_SIZE];
-
-event_queue_t event_queue_lowest;
-char event_queue_lowest_stack[VCRTOS_CONFIG_THREAD_EVENT_LOWEST_STACK_SIZE];
-
-typedef struct
-{
-    event_queue_t *queue;
-    char *stack;
-    size_t stack_size;
-    unsigned priority;
-} event_threads_t;
-
-const event_threads_t _event_threads[] = {
-    { &event_queue_highest, event_queue_highest_stack, sizeof(event_queue_highest_stack),
-      VCRTOS_CONFIG_THREAD_EVENT_HIGHEST_PRIORITY },
-    { &event_queue_medium, event_queue_medium_stack, sizeof(event_queue_medium_stack),
-      VCRTOS_CONFIG_THREAD_EVENT_MEDIUM_PRIORITY },
-    { &event_queue_lowest, event_queue_lowest_stack, sizeof(event_queue_lowest_stack),
-      VCRTOS_CONFIG_THREAD_EVENT_LOWEST_PRIORITY }
-};
-
-void event_thread_init(void *instance, event_queue_t *queue, char *stack, size_t stack_size, unsigned priority)
-{
-    event_queue_init(instance, queue);
-
-    (void) thread_create(instance, stack, stack_size, priority,
-                         THREAD_FLAGS_CREATE_WOUT_YIELD | THREAD_FLAGS_CREATE_STACKMARKER,
-                         thread_event_handler, queue, "event");
-}
-
-#endif // #if VCRTOS_CONFIG_THREAD_EVENT_ENABLE
-
 void kernel_init(void)
 {
     (void) cpu_irq_disable();
@@ -150,15 +97,6 @@ void kernel_init(void)
                          KERNEL_THREAD_PRIORITY_IDLE,
                          THREAD_FLAGS_CREATE_WOUT_YIELD | THREAD_FLAGS_CREATE_STACKMARKER,
                          thread_idle_handler, (void *)instance, "idle");
-
-#if VCRTOS_CONFIG_THREAD_EVENT_ENABLE
-    for (unsigned i = 0; i < (sizeof(_event_threads) / sizeof(_event_threads[0])); i++)
-    {
-        event_thread_init(instance, _event_threads[i].queue,
-                          _event_threads[i].stack, _event_threads[i].stack_size,
-                          _event_threads[i].priority);
-    }
-#endif
 
     cpu_switch_context_exit();
 }
